cd5ad2982da7b62311f5cf3b118e977671e8498d
hoss4 <mohamed2001hoss@gmail.com>
2024-11-24 10:34:22 +0200

    latest

--- src/main/java/com/kie/CustomWIH.java
+++ src/main/java/com/kie/CustomWIH.java
@@ -9,6 +9,9 @@ import javax.jms.MessageConsumer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.kie.api.runtime.KieSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.kie.api.runtime.process.WorkItem;
@@ -28,6 +31,7 @@ public class CustomWIH implements WorkItemHandler {
 
     private String connectionFactoryName;
     private String destinationName;
+    private KieSession kieSession;
     Map<String, Object> map = new HashMap<>();
 
     private ConnectionFactory connectionFactory;
@@ -36,13 +40,15 @@ public class CustomWIH implements WorkItemHandler {
 
 
     public CustomWIH(String connectionFactoryName,
-                                         String destinationName) {
+                     String destinationName, KieSession kieSession) {
         System.out.println("in constructor");
 
         this.connectionFactoryName = connectionFactoryName;
         this.destinationName = destinationName;
         System.out.println("Factory : " + connectionFactoryName);
         System.out.println("Destination : " + destinationName);
+        this.kieSession = kieSession;
+        System.out.println("Created session ");
         init();
         System.out.println("finished constructor");
     }
@@ -51,16 +57,13 @@ public class CustomWIH implements WorkItemHandler {
 
     protected void init() {
         try {
+
             System.out.println("in init");
-            logger.info("in init");
 
             InitialContext ctx = new InitialContext();
-            if (this.connectionFactory == null) {
-                this.connectionFactory = (ConnectionFactory) ctx.lookup(connectionFactoryName);
-            }
-            if (this.destination == null) {
-                this.destination = (Destination) ctx.lookup(destinationName);
-            }
+            this.connectionFactory = (ConnectionFactory) ctx.lookup(connectionFactoryName);
+            this.destination = (Destination) ctx.lookup(destinationName);
+
             logger.info("JMS based work item handler successfully activated on destination {}",
                     destination);
             System.out.println("finished init");
@@ -92,15 +95,15 @@ public class CustomWIH implements WorkItemHandler {
             consumer = session.createConsumer(destination);
             System.out.println("Created consumer ");
             System.out.println("consuming ");
-             message = consumer.receive(5000); // Wait for up to 5 second
-            System.out.println("message "+message);
+            message = consumer.receive(5000); // Wait for up to 5 second
+            System.out.println("consumed ");
             if (message instanceof TextMessage) {
                 TextMessage textMessage = (TextMessage) message;
                 String receivedMessage = textMessage.getText();
                 System.out.println("Received message: " + receivedMessage);
-
-                System.out.println("consumed ");
-
+                System.out.println("Starting Process Activition");
+                startJBPMProcess(receivedMessage);
+                System.out.println("Finished Process Activition");
                 manager.completeWorkItem(workItem.getId(),
                         map);
 
@@ -155,11 +158,52 @@ public class CustomWIH implements WorkItemHandler {
 
     }
 
-    public void close() {
-        connectionFactory = null;
-        destination = null;
 
-    }
+    private void startJBPMProcess(String messageContent) {
+
+
+        System.out.println("in  startJBPMProcess");
+        logger.info("Splitting message");
+
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+       try {
+           Map<String, String> message = objectMapper.readValue(messageContent, Map.class);
+
+           // Access fields easily
+           String sender = message.get("sender");
+           String time = message.get("time");
+           String process = message.get("process");
+           String data = message.get("data");
+
+
+           System.out.println("Decoded Message:");
+           System.out.println("Sender: " + sender);
+           System.out.println("Time: " + time);
+           System.out.println("Process: " + process);
+           System.out.println("Data: " + data);
+
+           logger.info("Decoded message");
+           // Create parameters map for the process
+
+           System.out.println("setting params ");
+           params.put("Sender", sender);
+           params.put("Time", time);
+           params.put("Process", process);
+           params.put("Data", data);
+           logger.info("Set message params");
+           System.out.println("Starting process with params: " + params);
+           kieSession.startProcess(process, params);
+           System.out.println("Finished starting process" );
+       }catch (Exception e) {
+           System.out.println("Encountered Exception : "+ e.getMessage());
+       }
+        // Start the process
+
+
+}
 
 
     public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
